e67b5e5144450c65aec7485386f7bc143dc0c602
fearm3 <ibraaa1628@gmail.com>
2022-01-02 22:15:24 +0300

    add action type

--- src/App.js
+++ src/App.js
@@ -16,16 +16,35 @@ const initialStore = {
 //reducer
 function reducer(state, action) {
   console.log({ state, action });
+  if (action.type === "DECREASE") {
+    // state.count === state.count - 1;
+    return { count: state.count - 1 };
+  }
+  if (action.type === "INCREASE") {
+    // state.count === state.count - 1;
+    return { count: state.count + 1 };
+  }
+  if (action.type === "RESET") {
+    // state.count === state.count - 1;
+    return { count: 0 };
+  }
   return state;
 }
 
 const store = createStore(reducer, initialStore);
+store.dispatch({ type: "DECREASE" });
+store.dispatch({ type: "DECREASE" });
+store.dispatch({ type: "RESET" });
+store.dispatch({ type: "INCREASE" });
+
+console.log(store.getState());
+
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar />
+      <Navbar cart={store.getState()} />
       <CartContainer cart={cartItems} />
     </main>
   );
