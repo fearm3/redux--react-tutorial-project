eb3b2e08140b1659a8a458ac338d8f186e49bbd6
fearm3 <ibraaa1628@gmail.com>
2022-01-03 19:36:04 +0300

    add toggle

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,11 @@
-import { DECREASE, INCREASE, CLEAR_CART, REMOVE, GET_TOTALS } from "./action";
+import {
+  DECREASE,
+  INCREASE,
+  CLEAR_CART,
+  REMOVE,
+  GET_TOTALS,
+  TOGGLE_AMOUNT,
+} from "./action";
 
 //reducer
 function reducer(state, action) {
@@ -56,6 +63,22 @@ function reducer(state, action) {
     );
     total = parseFloat(total.toFixed(2));
     return { ...state, total, amount };
+  }
+  if (action.type === TOGGLE_AMOUNT) {
+    return {
+      ...state,
+      cart: state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          if (action.payload.toggle === "inc") {
+            return (cartItem = { ...cartItem, amount: cartItem.amount + 1 });
+          }
+          if (action.payload.toggle === "dec") {
+            return (cartItem = { ...cartItem, amount: cartItem.amount - 1 });
+          }
+        }
+        return cartItem;
+      }),
+    };
   }
   return state;
   //
