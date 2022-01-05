8c881ddd1ddc60b22d4371a1a575ac8b71ae3ded
fearm3 <ibraaa1628@gmail.com>
2022-01-05 12:18:03 +0300

    add reductoolkit

--- redux_two/src/components/Counter.js
+++ redux_two/src/components/Counter.js
@@ -1,25 +1,27 @@
 import { useSelector, useDispatch } from "react-redux";
-import { INCREMENT } from "../store";
+
 import classes from "./Counter.module.css";
+
+import { counterActions } from "../store";
 
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
   const show = useSelector((state) => state.showCounter);
   const incrementHandler = () => {
-    dispatch({ type: INCREMENT });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(5)); //{type:SOME_UNIQUE_IDENTIFIER,payload:10}
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
